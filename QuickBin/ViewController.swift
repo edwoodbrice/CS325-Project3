41e2f8bf85a6056f588d67e780a06b947fd25cc3
User <lxhjhxx@1x-nat-vl932-172-30-225-58.wireless.umass.edu>
2018-11-12 08:00:54 -0500

    Initial commit.

---
+++ QuickBin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickBin
+//
+//  Created by User on 11/12/18.
+//  Copyright © 2018 cs325-project3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
